b38cdde620c0288a11482ff4e462db2882312f4b
PeterPletoKis <koritnak@gmail.com>
2014-10-20 01:20:03 +0200

    a
    a

---
+++ WikiParser/WikiParser/DisambiguationPageInfo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiParser
+{
+    public class DisambiguationPageInfo
+    {
+        public string title { get; set; }
+        public List<string> pagesTitles { get; set; } 
+        public List<PageInfo> pages { get; set; }
+
+        public DisambiguationPageInfo(string title, List<string> pagesTitles)
+        {
+            this.title = title;
+            this.pagesTitles = pagesTitles;
+            pages = new List<PageInfo>();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("<dspage>{0}", Environment.NewLine);
+            sb.AppendFormat("<title>{0}</title>{1}", title, Environment.NewLine);
+            foreach (PageInfo pageInfo in pages)
+            {
+                sb.Append(pageInfo);
+            }
+            sb.AppendFormat("</dspage>{0}", Environment.NewLine);
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ WikiParser/WikiParser/PageInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace WikiParser
+{
+    public class PageInfo
+    {
+        private string title;
+        private string anchor;
+        private string description;
+
+        public PageInfo(string title, string description)
+        {
+            this.title = title;
+            this.anchor = "http://sk.wikipedia.org/wiki/" + title.Replace(" ", "_");
+            this.description = description;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("<page>{0}", Environment.NewLine);
+            sb.AppendFormat("<title>{0}</title>{1}", title, Environment.NewLine);
+            sb.AppendFormat("<anchor>{0}</anchor>{1}", anchor, Environment.NewLine);
+            sb.AppendFormat("<description>{0}</description>{1}", description, Environment.NewLine);
+            sb.AppendFormat("</page>{0}", Environment.NewLine);
+
+            return sb.ToString();
+        }
+    }
+}
